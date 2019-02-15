e632bd863bdafb58d1926f8ce741260049f3329d
Mikołaj Zuziak <miki.zuziak@gmail.com>
2019-02-15 19:35:54 +0100

    Big update

--- sketch.js
+++ sketch.js
@@ -47,18 +47,90 @@ class Segment {
             return false;
         }
     }
+
+    setColor() {
+        this.fill = projectData.picekedColor;
+
+        if (!this.index && projectData.symetry) {
+            for (let s of projectData.segments) {
+                if (s.kartPos) {
+                    let sx = s.kartPos.x;
+                    let sy = s.kartPos.y;
+
+                    if (projectData.symX < 0 && projectData.symY > 0) {
+                        //Symetria X:
+                        if (sx == projectData.symY * (this.kartPos.x) && sy == projectData.symX * (this.kartPos.y - 1)) {
+                            s.fill = projectData.picekedColor;
+                            print('x');
+                            break;
+                        }
+                    }
+
+                    if (projectData.symY < 0 && projectData.symX > 0) {
+                        // Symetria Y:
+                        if (sx == projectData.symY * (this.kartPos.x + 1) && sy == projectData.symX * (this.kartPos.y)) {
+                            s.fill = projectData.picekedColor;
+                            print('y');
+                            break;
+                        }
+                    }
+
+                    if (projectData.symX < 0 && projectData.symY < 0) {
+                        // Symetria WŚ:
+                        if (sx == projectData.symY * (this.kartPos.x + 1) && sy == projectData.symX * (this.kartPos.y - 1)) {
+                            s.fill = projectData.picekedColor;
+                            print('xy');
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
 
 class Gui {
 
     createOptions() {
         projectData['axis'] = false;
-        projectData['controls'] = [];
+        projectData['symCh'] = [];
+        let spacer;
+        //////////////////////////////////////////////
+        spacer = createDiv("Osie układu:");
+        spacer.addClass('spacer');
+        select('.options').child(spacer);
 
-        let axCheck = createCheckbox('Osie', false);
-        axCheck.changed(this.axFlip);
-        select('.options').child(axCheck);
-        projectData.controls.push(axCheck);
+        projectData['axCheck'] = createCheckbox('Osie', false);
+        projectData.axCheck.changed(this.axFlip);
+        select('.options').child(projectData.axCheck);
+        //////////////////////////////////////////////
+        spacer = createDiv("Symetria:");
+        spacer.addClass('spacer');
+        select('.options').child(spacer);
+
+        let symX = createCheckbox('Symetria - oś X', false);
+        symX.attribute('onchange', 'projectData.symX *= -1');
+        symX.changed(this.symetry);
+        select('.options').child(symX);
+        projectData.symCh.push(symX);
+
+        let symY = createCheckbox('Symetria - oś Y', false);
+        symY.attribute('onchange', 'projectData.symY *= -1');
+        symY.changed(this.symetry);
+        select('.options').child(symY);
+        projectData.symCh.push(symY);
+
+        //////////////////////////////////////////////
+        spacer = createDiv("Reset planszy:");
+        spacer.addClass('spacer');
+        select('.options').child(spacer);
+
+        projectData['resetBtn'] = createButton('Reset');
+        projectData.resetBtn.mouseClicked(this.reset);
+        select('.options').child(projectData.resetBtn);
+
+
 
         return this;
     }
@@ -95,8 +167,8 @@ class Gui {
     createBoard() {
         projectData['segments'] = [];
 
-        let jTrack = projectData.segN / -2;
-        let iTrack = projectData.segN / -2;
+        // let jTrack = projectData.segN / -2;
+        // let iTrack = projectData.segN / -2;
 
         for (let j = 0; j < projectData.segN + 2; j++) {
             for (let i = 0; i < projectData.segN + 2; i++) {
@@ -116,6 +188,11 @@ class Gui {
                     j: j
                 }
 
+                let kartPos = {
+                    x: j - (projectData.segN + 2) / 2,
+                    y: -i + (projectData.segN + 2) / 2
+                }
+
                 if (((j == 0 || j == projectData.segN + 1) && i > 0 && i < projectData.segN + 1) ||
                     ((i == 0 || i == projectData.segN + 1) && j > 0 && j < projectData.segN + 1)) {
                     let s = new Segment(pos, dim, iJ, undefined, true);
@@ -123,17 +200,12 @@ class Gui {
                     s.stroke = 'pink';
                     projectData.segments.push(s);
                 } else if (j != 0 && j != projectData.segN + 1) {
-
-                    let kartPos = {
-                        x: jTrack,
-                        y: iTrack
-                    }
-
                     projectData.segments.push(new Segment(pos, dim, iJ, kartPos, false));
+                    // iTrack++;
                 }
-                iTrack++;
+                // iTrack++;
             }
-            jTrack++;
+            // jTrack++;
         }
 
         return this;
@@ -141,6 +213,24 @@ class Gui {
 
     axFlip() {
         projectData.axis = this.checked();
+    }
+
+    symetry() {
+        if (projectData.symCh[0].checked() || projectData.symCh[1].checked()) {
+            projectData.symetry = true;
+        } else {
+            projectData.symetry = false;
+        }
+    }
+
+    reset() {
+        for (let s of projectData.segments) {
+            if (!s.index) {
+                s.fill = projectData.colors[projectData.colors.length - 1];
+            } else {
+                s.fill = '#F64C72';
+            }
+        }
     }
 
 }
@@ -151,7 +241,10 @@ const projectData = {
     colors: ['green', 'deepskyblue', 'purple', 'khaki', 'red', 'greenyellow', 'black', 'white', 'saddlebrown', 'darkorange', '#C0C0C0'],
     litery: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
     spacer: 7,
-    picekedColor: '#C0C0C0'
+    picekedColor: '#C0C0C0',
+    symetry: false,
+    symX: 1,
+    symY: 1
 }
 
 function pick(color) {
@@ -186,8 +279,7 @@ function draw() {
 function mouseClicked() {
     for (s of projectData.segments) {
         if (s.checkPointing() && !s.index) {
-            s.fill = projectData.picekedColor;
-            print(s.kartPos);
+            s.setColor();
         }
     }
 }
