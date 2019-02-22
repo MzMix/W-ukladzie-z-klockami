2b7888bad9a1361a74579b7683980dcc07248634
Mikołaj Zuziak <miki.zuziak@gmail.com>
2019-02-22 16:43:54 +0100

    Big update 2.6

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
+Number.prototype.between = function (a, b) {
+    let minVal = min([a, b]);
+    let maxVal = max([a, b]);
+
+    return this > minVal && this < maxVal;
+};
+
 class Segment {
-    constructor(pos, dim, iJ, kartPos, index) {
+    constructor(pos, dim, iJ, kartPos, index, number) {
         this.pos = pos;
         this.dim = dim;
         this.kartPos = kartPos;
@@ -7,6 +14,9 @@ class Segment {
         this.iJ = iJ;
         this.fill = color('#C0C0C0');
         this.stroke = color(255, 255, 255, 125);
+        this.textColor = color(255, 255, 255);
+        this.number = number;
+        this.txt = this.number;
     }
 
     display() {
@@ -25,22 +35,23 @@ class Segment {
             let txt = this.iJ.i
 
             if (this.iJ.i == 0 || this.iJ.i == 11) {
-                txt = projectData.litery[this.iJ.j - 1];
+                txt = Global.litery[this.iJ.j - 1];
             }
 
             textSize(15);
-            fill(255);
-            stroke(255);
+            fill(this.textColor);
+            stroke(this.textColor);
             textAlign(CENTER, CENTER)
             strokeWeight(0);
             text(txt, 2, 2, this.dim.w, this.dim.h);
+
         } else {
             textSize(15);
             fill(0);
             stroke(255);
             textAlign(CENTER, CENTER)
             strokeWeight(0);
-            text(this.iJ.i * this.iJ.j, 2, 2, this.dim.w, this.dim.h);
+            text(this.txt, 2, 2, this.dim.w, this.dim.h);
         }
 
         pop();
@@ -56,34 +67,34 @@ class Segment {
     }
 
     setColor() {
-        this.fill = projectData.picekedColor;
+        this.fill = Global.picekedColor;
 
-        if (!this.index && projectData.symetry) {
-            for (let s of projectData.segments) {
+        if (!this.index && Global.symetry) {
+            for (let s of Global.segments) {
                 if (s.kartPos) {
                     let sx = s.kartPos.x;
                     let sy = s.kartPos.y;
 
-                    if (projectData.symX < 0 && projectData.symY > 0) {
+                    if (Global.symX < 0 && Global.symY > 0) {
                         //Symetria X:
-                        if (sx == projectData.symY * (this.kartPos.x) && sy == projectData.symX * (this.kartPos.y - 1)) {
-                            s.fill = projectData.picekedColor;
+                        if (sx == Global.symY * (this.kartPos.x) && sy == Global.symX * (this.kartPos.y - 1)) {
+                            s.fill = Global.picekedColor;
                             break;
                         }
                     }
 
-                    if (projectData.symY < 0 && projectData.symX > 0) {
+                    if (Global.symY < 0 && Global.symX > 0) {
                         // Symetria Y:
-                        if (sx == projectData.symY * (this.kartPos.x + 1) && sy == projectData.symX * (this.kartPos.y)) {
-                            s.fill = projectData.picekedColor;
+                        if (sx == Global.symY * (this.kartPos.x + 1) && sy == Global.symX * (this.kartPos.y)) {
+                            s.fill = Global.picekedColor;
                             break;
                         }
                     }
 
-                    if (projectData.symX < 0 && projectData.symY < 0) {
+                    if (Global.symX < 0 && Global.symY < 0) {
                         // Symetria WŚ:
-                        if (sx == projectData.symY * (this.kartPos.x + 1) && sy == projectData.symX * (this.kartPos.y - 1)) {
-                            s.fill = projectData.picekedColor;
+                        if (sx == Global.symY * (this.kartPos.x + 1) && sy == Global.symX * (this.kartPos.y - 1)) {
+                            s.fill = Global.picekedColor;
                             break;
                         }
                     }
@@ -106,16 +117,17 @@ class Gui {
 
         select('#closeBtn').mouseClicked(() => {
             select('#modal1').style('display', "none");
+            Global.modalOpened = false;
         });
 
         return this;
     }
 
-    generateOption(destination, dataStorage, spacerName, inputType, inputName, fxn, atr) {
+    generateOption(destination, dataStorage, spacerName, inputType, inputName, fxn, atr, listOptions) {
         let spacer, input;
 
-        if (!projectData.hasOwnProperty(dataStorage)) {
-            projectData[`${dataStorage}`] = [];
+        if (!Global.hasOwnProperty(dataStorage)) {
+            Global[`${dataStorage}`] = [];
         }
 
         if (spacerName) {
@@ -135,12 +147,27 @@ class Gui {
                 input.attribute('onchange', atr)
             }
 
-        } else {
+        } else if (inputType == 'button') {
             input = createButton(inputName);
             input.mouseClicked(fxn);
+
+            if (atr) {
+                input.attribute('onclick', atr)
+            }
+
+        } else if (inputType == 'list') {
+            input = createSelect();
+
+            if (listOptions) {
+                for (let opt of listOptions) {
+                    input.option(opt);
+                }
+            }
+
+            input.changed(fxn);
         }
 
-        projectData[dataStorage].push(input);
+        Global[dataStorage].push(input);
         select(destination).child(input);
     }
 
@@ -155,12 +182,15 @@ class Gui {
 
     createOptions() {
         this.generateOption(".o1", "axles", "Osie układu:", 'checkbox', "Osie", this.axFlip);
-        this.generateOption(".o1", 'symCh', "Symetria:", 'checkbox', "Symetria - oś X", this.symetry, 'projectData.symX *= -1');
-        this.generateOption(".o1", "symCh", undefined, 'checkbox', "Symetria - oś Y", this.symetry, 'projectData.symY *= -1');
+        this.generateOption(".o1", 'symCh', "Symetria:", 'checkbox', "Symetria - oś X", this.symetry, 'Global.symX *= -1');
+        this.generateOption(".o1", "symCh", undefined, 'checkbox', "Symetria - oś Y", this.symetry, 'Global.symY *= -1');
+        this.generateOption(".o1", "colorIndex", "Zmiana opisu pól", 'button', "Zmiana opisu", this.colorindex);
         this.generateOption(".o1", "dropBtn", "Opcje:", 'button', "Pokaż opcje", function () {
             select('#modal1').style('display', 'block');
+            Global.modalOpened = true;
         });
 
+        this.generateOption(this.createModalSection('list'), "textFill", "Zmiana opisu pól", 'list', "Zmiana opisu", this.changeTxt, undefined, ['Numeracja', 'Adresowanie', 'Tabliczka mnożenia', 'Brak opisów']);
         this.generateOption(this.createModalSection('reset'), "resetBtn", "Reset planszy:", 'button', "Reset", this.reset);
         this.generateOption(this.createModalSection('saveImg'), "saveImgBtn", "Zapis planszy do zdjęcia:", 'button', "Zapisz", this.saveImg);
 
@@ -168,58 +198,54 @@ class Gui {
     }
 
     saveImg() {
-
-        if (projectData.saveImgCounter > 0) {
-            saveCanvas(`mata${projectData.saveImgCounter}`, 'png');
-        } else {
-            saveCanvas(`mata`, 'png');
-        }
-
+        let data = new Date();
+        saveCanvas(`mata${data.getMinutes()}-${data.getSeconds()}`, 'png');
     }
 
     createPalette() {
 
-        projectData['palette'] = [];
+        Global['palette'] = [];
 
-        for (let col of projectData.colors) {
+        for (let col of Global.colors) {
 
             let div = createDiv();
 
             if (col == '#C0C0C0') {
                 div.html('G');
             }
+
             div.addClass('paletteBtn');
             div.style('background-color', col);
             div.attribute("onclick", `pick('${col}')`);
             select(".palette").child(div);
-            projectData.palette.push(div);
+            Global.palette.push(div);
         }
 
         return this;
     }
 
     createBox() {
-        let s = projectData.size * (projectData.segN + 2) + 11 * projectData.spacer + 6;
-        projectData['canva'] = createCanvas(s, s);
-        select(".box").child(projectData.canva);
+        let s = Global.size * (Global.segN + 2) + 11 * Global.spacer + 6;
+        Global['canva'] = createCanvas(s, s);
+        select(".box").child(Global.canva);
 
         return this;
     }
 
     createBoard() {
-        projectData['segments'] = [];
+        Global['segments'] = [];
 
-        for (let j = 0; j < projectData.segN + 2; j++) {
-            for (let i = 0; i < projectData.segN + 2; i++) {
+        for (let j = 0; j < Global.segN + 2; j++) {
+            for (let i = 0; i < Global.segN + 2; i++) {
 
                 let pos = {
-                    x: j * projectData.size + projectData.spacer * j + 2,
-                    y: i * projectData.size + projectData.spacer * i + 2
+                    x: j * Global.size + Global.spacer * j + 2,
+                    y: i * Global.size + Global.spacer * i + 2
                 }
 
                 let dim = {
-                    w: projectData.size,
-                    h: projectData.size
+                    w: Global.size,
+                    h: Global.size
                 }
 
                 let iJ = {
@@ -228,18 +254,19 @@ class Gui {
                 }
 
                 let kartPos = {
-                    x: j - (projectData.segN + 2) / 2,
-                    y: -i + (projectData.segN + 2) / 2
+                    x: j - (Global.segN + 2) / 2,
+                    y: -i + (Global.segN + 2) / 2
                 }
 
-                if (((j == 0 || j == projectData.segN + 1) && i > 0 && i < projectData.segN + 1) ||
-                    ((i == 0 || i == projectData.segN + 1) && j > 0 && j < projectData.segN + 1)) {
+                if (((j == 0 || j == Global.segN + 1) && i > 0 && i < Global.segN + 1) ||
+                    ((i == 0 || i == Global.segN + 1) && j > 0 && j < Global.segN + 1)) {
                     let s = new Segment(pos, dim, iJ, undefined, true);
                     s.fill = '#F64C72';
                     s.stroke = 'pink';
-                    projectData.segments.push(s);
-                } else if (j != 0 && j != projectData.segN + 1) {
-                    projectData.segments.push(new Segment(pos, dim, iJ, kartPos, false));
+                    Global.segments.push(s);
+                } else if (j != 0 && j != Global.segN + 1) {
+                    let s = new Segment(pos, dim, iJ, kartPos, false);
+                    Global.segments.push(s);
                 }
             }
         }
@@ -248,48 +275,106 @@ class Gui {
     }
 
     axFlip() {
-        projectData.axis = this.checked();
+        Global.axis = this.checked();
     }
 
     symetry() {
-        if (projectData.symCh[0].checked() || projectData.symCh[1].checked()) {
-            projectData.symetry = true;
+        if (Global.symCh[0].checked() || Global.symCh[1].checked()) {
+            Global.symetry = true;
         } else {
-            projectData.symetry = false;
+            Global.symetry = false;
         }
     }
 
     reset() {
-        for (let s of projectData.segments) {
+        for (let s of Global.segments) {
             if (!s.index) {
-                s.fill = projectData.colors[projectData.colors.length - 1];
+                s.fill = Global.colors[Global.colors.length - 1];
             } else {
-                s.fill = '#F64C72';
+                s.fill = '#F64C72'
+                s.stroke = 'pink';
+                s.textColor = color(255, 255, 255);
             }
         }
     }
 
-    // colorindex() {
-    //     let ind = 0;
-    //     for (let j = 0; j < projectData.segN + 2; j++) {
-    //         for (let i = 0; i < projectData.segN + 2; i++) {
-    //             if (projectData.segments[ind].index) {
+    colorindex() {
+        for (let s of Global.segments) {
+            if (s.index) {
+                if (s.iJ.i.between(0, 11)) {
 
-    //                 if ((j == 0 || j == projectData.segN + 1) && i > 0 && i < projectData.segN + 1) {
-    //                     projectData.segments[ind].fill = projectData.colors[j];
-    //                 }
-    //                 if ((i == 0 || i == projectData.segN + 1) && j > 0 && j < projectData.segN + 1) {
-    //                     projectData.segments[ind].fill = projectData.colors[i];
-    //                 }
-    //             }
-    //             ind++;
-    //         }
-    //     }
-    // }
+                    if (s.fill == '#F64C72' && s.stroke == 'pink') {
+                        s.fill = Global.colors[abs(s.iJ.i - 1)];
+                        s.stroke = color(0, 0, 0, 0);
+                        s.textColor = color(0, 0, 0, 0);
+                    } else {
+                        s.fill = '#F64C72'
+                        s.stroke = 'pink';
+                        s.textColor = color(255, 255, 255);
+                    }
+                } else if (s.iJ.j.between(0, 11)) {
+                    if (s.fill == '#F64C72' && s.stroke == 'pink') {
+                        s.fill = Global.colors[abs(s.iJ.j - 1)];
+                        s.stroke = color(0, 0, 0, 0);
+                        s.textColor = color(0, 0, 0, 0);
+                    } else {
+                        s.fill = '#F64C72'
+                        s.stroke = 'pink';
+                        s.textColor = color(255, 255, 255);
+                    }
+                }
+            }
+        }
+    }
 
+    changeTxt() {
+        let type = this.value();
+        let eql;
+
+        switch (type) {
+            case 'Numeracja':
+                eql = 's.number';
+                break;
+
+            case 'Adresowanie':
+                eql = 'Global.litery[s.iJ.j - 1] + s.iJ.i';
+                break;
+
+            case 'Tabliczka mnożenia':
+                eql = 's.iJ.i * s.iJ.j';
+                break;
+
+            case 'Brak':
+                eql = 'undefined';
+                break;
+
+            default:
+                eql = 's.number';
+                break;
+        }
+
+        for (let s of Global.segments) {
+            if (!s.index) {
+                s.txt = eval(eql);
+            }
+        }
+    }
+
+    addNumber() {
+        let total = 1;
+        for (let s of Global.segments) {
+            if (!s.index) {
+                s.number = total;
+                s.txt = total;
+                total++;
+            }
+        }
+
+        return this;
+    }
 }
 
-const projectData = {
+const Global = {
     size: 40,
     segN: 10,
     colors: ['green', 'deepskyblue', 'purple', 'khaki', 'red', 'greenyellow', 'black', 'white', 'saddlebrown', 'darkorange', '#C0C0C0'],
@@ -301,28 +386,30 @@ const projectData = {
     symCh: [],
     symX: 1,
     symY: 1,
-    saveImgCounter: 0
+    indexColored: 0,
+    modalOpened: false
 }
 
 function pick(color) {
-    projectData.picekedColor = color;
+    Global.picekedColor = color;
 }
 
 function setup() {
-    projectData['gui'] = new Gui();
-    projectData.gui.createPalette().createBox().createBoard().createModal().createOptions();
+    cursor('pointer');
+    Global['gui'] = new Gui();
+    Global.gui.createPalette().createBox().createBoard().addNumber().createModal().createOptions();
 }
 
 function draw() {
     background(color('#553D67'));
 
-    if (projectData.segments) {
-        for (let s of projectData.segments) {
+    if (Global.segments) {
+        for (let s of Global.segments) {
             s.display();
         }
     }
 
-    if (projectData.axis) {
+    if (Global.axis) {
         push();
         translate(-1 / 2, -1 / 2);
         stroke('yellow');
@@ -334,9 +421,11 @@ function draw() {
 }
 
 function mouseClicked() {
-    for (s of projectData.segments) {
-        if (s.checkPointing() && !s.index) {
-            s.setColor();
+    if (!Global.modalOpened) {
+        for (s of Global.segments) {
+            if (s.checkPointing() && !s.index) {
+                s.setColor();
+            }
         }
     }
 }
