f78a85a3b2dd9c41f45d246789b87e1e6ea1689e
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-08-10 21:16:23 +0200

    feat(Sharing): :sparkles: add exporting image of the board
    add exporting an image of the board to a .png file with a name containig current time

--- src/utils/SharingUtilities.js
+++ src/utils/SharingUtilities.js
@@ -11,3 +11,29 @@ export function DonloadContent(content, fileName, contentType) {
     a.download = fileName;
     a.click();
 }
+
+export function DownloadCanvas(canvas, filename) {
+    var anchor = document.createElement("a");
+    anchor.href = canvas.toDataURL("image/png");
+    anchor.download = `${filename}.png`;
+    anchor.click();
+
+    anchor.remove();
+}
+
+function AddLeadingZero(value) {
+    return value < 10 ? `0${value}` : value;
+}
+
+export function GetDateForFileName() {
+    const date = new Date();
+    const [year, month, day, hour, minutes, seconds] = [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
+
+    const eMonth = AddLeadingZero(month);
+    const eDay = AddLeadingZero(day);
+    const eHour = AddLeadingZero(hour);
+    const eMinutes = AddLeadingZero(minutes);
+    const eSeconds = AddLeadingZero(seconds);
+
+    return `${year}-${eMonth}-${eDay}-${eHour}-${eMinutes}-${eSeconds}`;
+}
