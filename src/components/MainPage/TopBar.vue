07e1dd69cfc4a9312daae68af762339ce5c8ca87
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-09-16 12:24:35 +0200

    fix(UI): :bug: fix right margin on top bar
    fix right margin on top bar to make it even

--- src/components/MainPage/TopBar.vue
+++ src/components/MainPage/TopBar.vue
@@ -12,8 +12,7 @@ const icons = ['palette', 'brush', 'share', 'gear'];
 </script>
 
 <template>
-    <div
-        class="col-12 d-flex flex-row flex-nowrap justify-content-between bg-purple text-white pt-1 pb-1">
+    <div class="col-12 d-flex flex-row flex-nowrap justify-content-between bg-purple text-white pt-1 pb-1">
 
         <div :class="TopBarElement" class="d-flex flex-row gap-2">
             <button v-for="(icon, index) in icons" :key="index" @click="SwitchMenu(index)"
@@ -26,7 +25,7 @@ const icons = ['palette', 'brush', 'share', 'gear'];
             <BoardName />
         </div>
 
-        <div :class="TopBarElement">W układzie z klockami <i class="bi bi-bricks"></i></div>
+        <div :class="TopBarElement" class="me-3">W układzie z klockami <i class="bi bi-bricks"></i></div>
 
     </div>
 </template>
