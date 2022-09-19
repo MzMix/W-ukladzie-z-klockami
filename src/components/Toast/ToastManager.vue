b3131ff3962ef6027323956dfeb22ef2c875a21f
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-09-19 14:00:15 +0200

    feat(UI): :sparkles: add Toast for importing color palette
    add Toast notification for importing color palette from .json file

--- src/components/Toast/ToastManager.vue
+++ src/components/Toast/ToastManager.vue
@@ -5,10 +5,7 @@ import { inject, onMounted } from 'vue';
 const ShowToast = inject('ToastTrigger');
 
 onMounted(() => {
-
-    for (let i = 1; i <= 8; i++) {
-        ShowToast(`#t${i}`)
-    }
+    // ShowToast(`#ColorPalettesAdded`)
 
 })
 
@@ -19,29 +16,11 @@ onMounted(() => {
     <div aria-live="polite" aria-atomic="true" class="" id="ToastGroup">
         <div class="toast-container bottom-0 end-0 p-3">
 
-            <bsToast Theme="white" Name="t1">
-                <template #header>Toast #1</template>
-            </bsToast>
-            <bsToast Theme="primary" Name="t2">
-                <template #header>Toast #2</template>
-            </bsToast>
-            <bsToast Theme="secondary" Name="t3">
-                <template #header>Toast #3</template>
-            </bsToast>
-            <bsToast Theme="purple" Name="t4">
-                <template #header>Toast #4</template>
-            </bsToast>
-            <bsToast Theme="success" Name="t5">
-                <template #header>Toast #5</template>
-            </bsToast>
-            <bsToast Theme="danger" Name="t6">
-                <template #header>Toast #6</template>
-            </bsToast>
-            <bsToast Theme="warning" Name="t7">
-                <template #header>Toast #7</template>
-            </bsToast>
-            <bsToast Theme="info" Name="t8">
-                <template #header>Toast #8</template>
+            <bsToast Theme="purple" Name="ColorPalettesAdded">
+                <template #header>Dodano nową paletę kolorów!</template>
+                <template #time>Teraz</template>
+                <template #body>Aby zmienić wybraną paletę przejdź do:<br /> <strong>Rysowanie > Zmiana palety
+                        kolorów</strong></template>
             </bsToast>
 
         </div>
