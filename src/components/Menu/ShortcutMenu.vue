1f0fcc1af25c532f3a349a210e299902b43a512e
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-09-25 19:56:38 +0200

    fix(UI): :lipstick: fix shortcut list flex

--- src/components/Menu/ShortcutMenu.vue
+++ src/components/Menu/ShortcutMenu.vue
@@ -37,14 +37,13 @@ const ListContent = [
 <template>
     <div class="text-center p-2 w-100 ps-3">
         <h4 class="mt-2 mb-4">Skróty Klawiszowe <i class="bi bi-keyboard"></i></h4>
-        //TMP//<br /><br />
 
         <ul class="list-group text-start">
 
-            <li class="list-group-item d-flex flex-row flex-wrap justify-content-between gap-1"
+            <li class="list-group-item d-flex flex-xl-row flex-column flex-wrap justify-content-between gap-1"
                 v-for="(entry, index) in ListContent" :key="index">
                 <span class="">{{entry.name}}:</span>
-                <span class="ps-1"><kbd><kbd>{{entry.modifier}}</kbd> + {{entry.key}}</kbd></span>
+                <span class=""><kbd><kbd>{{entry.modifier}}</kbd> + {{entry.key}}</kbd></span>
             </li>
 
         </ul>
